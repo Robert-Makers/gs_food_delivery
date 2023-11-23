9602ec0a5889d305d9818322b755d4f61d033571
Robert Taylor <rjt903@gmail.com>
2023-11-23 12:34:44 +0000

    Initial commit

---
+++ tests/test_order.py
@@ -0,0 +1,25 @@
+from src.order import Order
+
+'''
+I can return a menu of items and prices
+'''
+def test_return_menu():
+    order = Order()
+    assert order.show_menu() == f''
+
+'''
+Given I want to order
+I can add items to my basket
+'''
+
+
+'''
+To confirm my order
+I can see an itemised list of items and the total price
+'''
+
+
+'''
+Given I place an order
+I recieve a text to confirm order and delivery time
+'''
